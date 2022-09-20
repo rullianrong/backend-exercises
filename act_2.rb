6f7ec83956589739a0c41fab688ed4fdb93f2ebd
Rullian Rong <naillur.gnor@gmail.com>
2022-09-20 17:51:39 +0800

    Solved Acitivy 2

--- act_2.rb
+++ act_2.rb
@@ -6,7 +6,7 @@
 # We have given you an array and a number. Write a program that checks to see if the number appears in the array.
 
 arr = [1,3,5,7,9,11]
-number = 0
+number = 3
 
 checker = false
 arr.each do |num|
@@ -22,14 +22,39 @@ puts checker
 # Write a program that takes a number from the user between 0 and 100 and reports back whether the number is between 0 and 50, 51 and 100, or above 100.
 
 puts "Please input a number between 0 - 100:"
-userNum = gets.chomp.to_i
+user_num = gets.chomp.to_i
 
-if userNum >= 0 && userNum <= 50
+if user_num >= 0 && user_num <= 50
     puts "Your number is between 0 and 50!"
-elsif userNum >= 51  && userNum <= 100
+elsif user_num >= 51  && user_num <= 100
     puts "Your number is between 51 and 100!"
-elsif userNum > 100
+elsif user_num > 100
     puts "Your number is above 100!"
-elsif userNum < 0
+elsif user_num < 0
     puts "That's a negative number!"
 end
+
+# Problem no. 3
+# Write a while loop that takes input from the user, performs an action, and only stops when the user types "STOP". Each loop can get infor from the user.
+
+user_input = ''
+
+while user_input != "STOP"
+    puts "Please let us know more about you or type 'STOP' to stop:"
+    user_input = gets.chomp
+end
+
+# Problem no. 4
+# We have given you an array. Write a program that checks to see if the number is divisible by 2 and store them into a new array.
+
+arr = [6,3,1,8,4,2,10,65,102]
+
+divisible_by_two = []
+
+arr.each do |num|
+    if num % 2 == 0
+        divisible_by_two.push num
+    end
+end
+
+puts divisible_by_two
