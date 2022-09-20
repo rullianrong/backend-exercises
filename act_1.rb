723865faea308a9c182e80e6bd937f857dd7ca23
Rullian Rong <naillur.gnor@gmail.com>
2022-09-20 16:16:30 +0800

    Added instruction for each exercise

--- act_1.rb
+++ act_1.rb
@@ -1,13 +1,16 @@
-# # # activity no. 1
+# activity no. 1
+# Use the each method of Array to iterate over [1,2,3,4,5,6,7,8,9,10], and print out each value.
 [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].each { |n| puts "Number: #{n}" }
 
-# # # activity no. 2
+# activity no. 2
+# Suppose you have a hash h = {a:1, b:2, c:3, d:4}. Get the value of key :b. Add key:value pair {e:5} to this hash
 h = {a:1, b:2, c:3, d:4}
 puts h[:b]
 puts h[:e] = 5
 puts h
 
-# # activity no. 3
+# activity no. 3
+# Given the following data structures. Write a program that copies the information from the array into the empty hash that applies to the correct person.
 contact_data = [["john@email.com", "123 Main st.", "555-123-4567"], ["avion@email.com", "484 Not Found Dr.", "123-234-3454"]]
 
 contacts = {"John Cruz" => {}, "Avion School" => {}}
@@ -24,7 +27,8 @@ end
 
 puts contacts
 
-# # # activity no. 4
+# activity no. 4
+# Write a program called age.rb that asks the user how old they are and then tells them how old they will be in 10, 20, 30, and 40, years. 
 puts "How old are you?"
 age = gets.chomp
 
