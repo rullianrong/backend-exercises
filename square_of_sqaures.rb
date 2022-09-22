fa060b11f47a15873cdfd019eedab09cb7c4b07e
Rullian Rong <naillur.gnor@gmail.com>
2022-09-22 19:25:24 +0800

    refactored my code to make each and if block 1 liner

--- square_of_sqaures.rb
+++ square_of_sqaures.rb
@@ -1,22 +1,17 @@
 # Given an integral number, determine if it's a square number.
 
+# Solution using digital root
 def get_digital_root(number) 
     sum = 0
-    number.digits.each do |digit| 
-        sum += digit
-    end
-    if sum.to_s.length != 1
-        get_digital_root(sum)
-    else
-        sum
-    end
+    number.digits.each { |digit| sum += digit }
+    sum.to_s.length != 1 ? get_digital_root(sum) : sum
 end
 
 puts "Enter a number to check if it's a perfect square:"
 number = gets.chomp
 
 if [1,4,7,9].include? get_digital_root(number.to_i)
-    puts "This number is a perfect square" 
+    puts "This number is a perfect square." 
 else
-    puts "Sorry, but this is not a perfect square"
+    puts "Sorry, but this is not a perfect square."
 end
